06825d64b804e56dc18d801e04f0e4a9099cc821
TiegiserII <ublazem@gmail.com>
2014-11-11 19:23:57 -0600

    Update users.sql

--- SQL_files/users.sql
+++ SQL_files/users.sql
@@ -94,3 +94,27 @@ VALUES
 ('patty mayonaisse', 'password', '1234567891235551', 'Paula', 'Jarrod',
    '220 Arabian St.', 'Austin', 'Texas', '82549');
 
+--
+-- Donald's users
+--
+
+INSERT INTO `ecom`.`user`
+(`username`, `password`, `credit_card`, `first_name`, `last_name`, `street`,
+	`city`, `state`, `zip`)
+VALUES
+('blackJesus', 'password', '123456789123334', 'Bob', 'White',
+   '312 Stonewall St.', 'Whitesville', 'Alaska', '38152');
+
+INSERT INTO `ecom`.`user`
+(`username`, `password`, `credit_card`, `first_name`, `last_name`, `street`,
+	`city`, `state`, `zip`)
+VALUES
+('Putin2018', 'password', '1234567891233332', 'Barak', 'OBama',
+   '2014 Crimea Dr.', 'Kenya', 'Hawaii', '88243');
+
+INSERT INTO `ecom`.`user`
+(`username`, `password`, `credit_card`, `first_name`, `last_name`, `street`,
+	`city`, `state`, `zip`)
+VALUES
+('yourLeftNostril', 'password', '1234567891233331', 'Collin', 'Themarines',
+   '220 Nazil St.', 'Houston', 'Texas', '82549');
