a29b93b603f57452fd91a4b8b0b102c1ca432b49
kc0344 <kc0344@users.noreply.github.com>
2014-11-12 17:47:03 -0600

    Update users.sql

--- SQL_files/users.sql
+++ SQL_files/users.sql
@@ -118,3 +118,28 @@ INSERT INTO `ecom`.`user`
 VALUES
 ('yourLeftNostril', 'password', '1234567891233331', 'Collin', 'Themarines',
    '220 Nazil St.', 'Houston', 'Texas', '82549');
+
+--
+-- Kai's users
+--
+
+INSERT INTO `ecom`.`user`
+(`username`, `password`, `credit_card`, `first_name`, `last_name`, `street`,
+	`city`, `state`, `zip`)
+VALUES
+('Mamamia555', 'password', '1234567891235993', 'Josh', 'Brady',
+   '10550 North Torrey Pines Rd.', 'La Jolla', 'California', '92037');
+
+INSERT INTO `ecom`.`user`
+(`username`, `password`, `credit_card`, `first_name`, `last_name`, `street`,
+	`city`, `state`, `zip`)
+VALUES
+('@Sugardady@', 'password', '1234567891235994', 'Tony', 'Johnson',
+   '121 N. LaSalle St.', 'Chicago', 'Illinois', '60602');
+
+INSERT INTO `ecom`.`user`
+(`username`, `password`, `credit_card`, `first_name`, `last_name`, `street`,
+	`city`, `state`, `zip`)
+VALUES
+('Bigmon123', 'password', '1234567891235999', 'Kevin', 'Jones',
+   '2006 East 4th St.', 'Austin', 'Texas', '78702');
