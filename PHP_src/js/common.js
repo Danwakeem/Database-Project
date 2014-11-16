aef4d244bfa2f465bd4e88cddc76dc15290ac297
Dan Jarvis <danwakeem@gmail.com>
2014-11-16 15:32:36 -0600

    delete in shopping cart is working now

--- PHP_src/js/common.js
+++ PHP_src/js/common.js
@@ -1,6 +1,5 @@
 //Need to insert posts after the product-post class rather than the rows. maybe new class?
 var addMorePosts = function(jsonObj){
-	console.log(jsonObj);
 	var obj = JSON.parse(jsonObj);
 	$.each(obj, function(index,value){
 		var html = "<div class=\"product-posts\"><div class=\"row\"><div class=\"col-md-4\"><a href=\"product_page.php?id=" + value['id'] + "\"><img src=\"" + value['featured_img'] + "\" style=\"width:100%;height:100%;\"></a></div><div class=\"col-md-8\"><h3><a href=\"product_page.php?id=" + value['id'] + "\">" + value['prod_name'] + "</a></h2><hr><p>" + value['prod_desc'] + "</p></div></div></div>";
@@ -10,13 +9,30 @@ var addMorePosts = function(jsonObj){
 }
 
 var addToCart = function(jsonObj){
-	console.log(jsonObj);
 	var obj = JSON.parse(jsonObj);
 	$.each(obj, function(index,value){
-		var html = '<div class="product-posts"><div class="row"><div class="col-md-4"><div class="row"><a href="product_page.php?id=' + value['id'] + '"><img src="' + value['featured_img'] + '" style="width:100%;height:100%;"></a></div></div><div class="col-md-8"><h4 style="margin-top: 0px;"><a href="product_page.php?id=' + value['id'] + '">' + value['prod_name'] + '</a></h4><p>Price: ' + value['price'] + '</p><a class="shopping-rm btn btn-danger" href="#"><i class="fa fa-trash-o fa-lg"></i> Delete</a></div></div></div><p id="prodId" style="display: none;">' + value['id'] + '</p><hr>';
+		var html = '<div class="product-posts"><div class="row"><div class="col-md-4"><div class="row"><a href="product_page.php?id=' + value['id'] + '"><img src="' + value['featured_img'] + '" style="width:100%;height:100%;"></a></div></div><div class="col-md-8"><h4 style="margin-top: 0px;"><a href="product_page.php?id=' + value['id'] + '">' + value['prod_name'] + '</a></h4><p>Price: ' + value['price'] + '</p><a class="shopping-rm btn btn-danger" onclick="rmCart(' + value['id'] +');"><i class="fa fa-trash-o fa-lg"></i> Delete</a></div></div><p class="prodId" style="display: none;">' + value['id'] + '</p><hr></div>';
 		$('.modal-body').append(html);
 	});
 }
+
+var rmCart = function(prodId){
+	console.log(prodId);
+	$.ajax({url: 'get_products.php',
+		data: {action: 'rmCart', proId: prodId},
+		type: 'post',
+		success: function(output){
+			$('.prodId').each(function(index){
+				var test = $( this ).text();
+				console.log(test);
+				if(Number(test) === prodId){
+					$(this).parent().remove();
+				}
+			});
+		}
+	});
+}
+
 
 var toType = function(obj) {
   return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
@@ -25,12 +41,10 @@ var toType = function(obj) {
 $(document).ready(function(){
 	$('.profile-pic').ready(function(){
 		var userId = $('.profile-pic').data('userid');
-		console.log(userId);
 		$.ajax({url: 'get_products.php',
 			 data: {action: 'profile-pic', usrId: userId},
 			 type: 'post',
 			 success: function(output){
-			 		console.log(output);
 			 		$('.profile-pic').attr('src',output);
 			 }
 		});
@@ -45,17 +59,6 @@ $(document).ready(function(){
 			 }
 			});
 		}
-	});
-	$('.shopping-rm').click(function(){
-		var prodId = $('#prodId').text();
-		$.ajax({url: 'get_products.php',
-			 data: {action: 'rmCart', proId: prodId},
-			 type: 'post',
-			 success: function(output){
-			 		var par = $('#prodId').parent();
-			 		par.remove();
-			 }
-			});
 	});
 	$('.add-cart').click(function(){
 		var prodId = $('#prod-id').text();
@@ -96,6 +99,18 @@ $(document).ready(function(){
          success: function(output) {
                       addMorePosts(output);
                   }
+		});
+	});
+	$('.shopping-rm').click(function(){
+		var prodId = $('#prodId').text();
+		console.log(prodId);
+		$.ajax({url: 'get_products.php',
+			 data: {action: 'rmCart', proId: prodId},
+			 type: 'post',
+			 success: function(output){
+			 		var par = $('#prodId').parent();
+			 		par.remove();
+			 }
 		});
 	});
 	$("[rel=tooltip]").tooltip({ placement: 'bottom'});
