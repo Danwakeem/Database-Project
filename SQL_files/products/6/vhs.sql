a7acc64b8638e81e941ed2146858bdf10050cd60
Matthew Sherwood <matthewsherwood@my.unt.edu>
2014-11-18 12:20:41 -0600

    Added products from users 4,5, and 6

---
+++ SQL_files/products/6/vhs.sql
@@ -0,0 +1,69 @@
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Memento',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Talladega Nights',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Pride and Prejudice',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Johnny English',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Hot Rod',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Godzilla',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('The Naked Gun',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Star Wars: Episode IV',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('Ratatoille',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
+
+INSERT INTO `ecom`.`product`
+(`prod_name`, `prod_desc`, `user_id`)
+VALUES
+('The Italian Job',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6);
