a82216c9cbde9c52d08deece7c705dc0ec8bfb4f
kc0344 <kc0344@users.noreply.github.com>
2014-11-25 00:30:27 -0600

    Create kai_orders.sql

---
+++ SQL_files/product_orders/kai_orders.sql
@@ -0,0 +1,45 @@
+-- 
+-- order id# 10 - 12
+-- 
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(10,
+192);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(10,
+235);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(11,
+181);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(11,
+225);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(12,
+189);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(12,
+220);
