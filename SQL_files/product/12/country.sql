81540da23e5310fe8a9d5afb8bb1d9c7763de6f7
kc0344 <kc0344@users.noreply.github.com>
2014-11-25 00:24:10 -0600

    Create country.sql

---
+++ SQL_files/product/12/country.sql
@@ -0,0 +1,88 @@
+-- 
+-- id# 236 - 240
+-- 
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(236,
+'Old Boots, New Dirt',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+9.9,
+12,
+1,
+'img_default/12/featured-products/236.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(237,
+'Anything Goes',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+11.9,
+12,
+0,
+'img_default/12/featured-products/237.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(238,
+'Montevallo',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+9.9,
+12,
+0,
+'img_default/12/featured-products/238.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(239,
+'Platinum',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+12.9,
+12,
+1,
+'img_default/12/featured-products/239.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(240,
+'The Outsiders',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+7.9,
+12,
+0,
+'img_default/12/featured-products/240.jpg');
