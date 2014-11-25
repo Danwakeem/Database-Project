a5cdf7d6eeca065bd570443b0ffe234875c927ca
kc0344 <kc0344@users.noreply.github.com>
2014-11-24 21:30:29 -0600

    Update pop.sql

--- SQL_files/product_category/pop.sql
+++ SQL_files/product_category/pop.sql
@@ -1,1 +1,104 @@
-1
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(181,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(182,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(183,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(184,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(185,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(186,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(187,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(188,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(189,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(190,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(191,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(192,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(193,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(194,
+'Pop');
+
+INSERT INTO `ecom`.`product_category`
+(`product_id`,
+`category_id`)
+VALUES
+(195,
+'Pop');
