c0b5db57b5ddec69adfb42dfda55274425ac5f71
kc0344 <kc0344@users.noreply.github.com>
2014-11-24 23:44:15 -0600

    Create rock.sql

---
+++ SQL_files/product/11/rock.sql
@@ -0,0 +1,87 @@
+-- 
+-- id# 201 - 205
+-- 
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(201,
+'No Fixed Address',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+6.9,
+11,
+1,
+'img_default/10/featured-products/201.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(202,
+'Echo of Miles: Scattered Tracks Across the Path',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+20.9,
+11,
+0,
+'img_default/11/featured-products/202.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(203,
+'Back In Black',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+5.9,
+11,
+0,
+'img_default/11/featured-products/203.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(204,
+'Songs of Innocence +',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+5.9,
+11,
+0,
+'img_default/11/featured-products/204.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(205,
+'Sonic Highways',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+8.9,
+11,
+0,
+'img_default/11/featured-products/205.jpg');
