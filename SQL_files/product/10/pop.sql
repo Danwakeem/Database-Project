2483b97101dd224d02ebc3e2ead29b53a9a72b3d
kc0344 <kc0344@users.noreply.github.com>
2014-11-24 23:12:28 -0600

    Update pop.sql

--- SQL_files/product/10/pop.sql
+++ SQL_files/product/10/pop.sql
@@ -1,1 +1,88 @@
-1
+-- 
+-- id# 181 - 185
+--
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(181,
+'1989',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+12.9,
+10,
+1,
+'img_default/10/featured-products/181.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(182,
+'PRISM',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+12.9,
+10,
+0,
+'img_default/10/featured-products/182.jpg');
+
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(183,
+'V',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+12.9,
+10,
+0,
+'img_default/10/featured-products/183.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(184,
+'21',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+9.9,
+10,
+0,
+'img_default/10/featured-products/184.jpg');
+
+INSERT INTO `ecom`.`product`
+(`id`,
+`prod_name`,
+`prod_desc`,
+`price`,
+`user_id`,
+`sold`,
+`featured_image`)
+VALUES
+(185,
+'The Essential Michael Jackson',
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean scelerisque ipsum mi, id laoreet diam cursus et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mauris augue, blandit nec scelerisque a, euismod non sem. Nulla vul.',
+16.9,
+10,
+1,
+'img_default/10/featured-products/184.jpg');
