a59fff6a0b878220d34a482a06d003cf1aec57ee
TiegiserII <ublazem@gmail.com>
2014-12-01 17:04:24 -0600

    Create donald_orders.sql

---
+++ SQL_files/product_orders/donald_orders.sql
@@ -0,0 +1,38 @@
+--
+-- order id# 7 - 9
+--
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(1,
+61);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(7,
+151);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(8,
+163);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(9,
+157);
+
+INSERT INTO `product_orders`
+(`order_id`,
+`product_id`)
+VALUES
+(9,
+158);
